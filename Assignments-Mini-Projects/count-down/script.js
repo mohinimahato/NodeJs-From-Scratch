c26aa3af81d81466d0fb08bfbd2b6d96b65429dd
Mohini Mahato <mohinimahatowork@gmail.com>
2023-03-18 22:57:13 +0530

    counter working

--- Assignments-Mini-Projects/count-down/script.js
+++ Assignments-Mini-Projects/count-down/script.js
@@ -1,24 +1,37 @@
 // Fetch Input Onclick of Button 
 // Split it into parts
 // showing the date and time on the ui 
-// set time out starts
+// setTimeInterval and clearTimeOut 
+
+const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
 
 function submitDateTime() {
-    // Capturing all the id's which will be updated
-    var dateInp = document.getElementById('dateInput').value;
-    var timeInp = document.getElementById('timeInput').value;
+    var dateTimeInput = document.getElementById('dateInput').value;
+    var countDown = new Date(dateTimeInput).getTime(),
+        x = setInterval(function () {
 
-    // Splitting the time and date input into arrays
-    var dateSplit = dateInp.split('-');
-    var timeSplit = timeInp.split(':');
+            var now = new Date().getTime(),
+                distance = countDown - now;
 
-    // Showing the output array values in the UI
-    dateOutput.innerHTML = dateSplit[2];
-    monthOutput.innerHTML = dateSplit[1];
-    yearOutput.innerHTML = dateSplit[0];
-    minOutput.innerHTML = timeSplit[0];
-    secOutput.innerHTML = timeSplit[1];
+            document.getElementById('days').innerHTML = Math.floor(distance / (day)),
+                document.getElementById('hours').innerHTML = Math.floor((distance % (day)) / (hour)),
+                document.getElementById('minutes').innerHTML = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById('seconds').innerHTML = Math.floor((distance % (minute)) / second);
 
-    // 
+            //do something later when date is reached
+            if (distance <= 0) {
+                clearInterval(x);
+                var eventSuccess = document.getElementById('eventSuccess');
+                eventSuccess.innerHTML = 'Count Down Completed'
+                document.getElementById('counterDiv').style.display = "none";
 
+
+            }
+
+        }, second)
 }
+
